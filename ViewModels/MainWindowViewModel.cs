794e6794cab1203b94624aaa8f630dfa45014294
Timboy67678 <me@timboy67678.com>
2025-10-26 00:16:19 +1000

    more updates

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.ObjectModel;
 
 namespace GoombaCast.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string WindowTitle => "GoombaCast connected to: yeah";
+        [ObservableProperty]
+        private string _windowTitle;
 
+        [ObservableProperty]
+        private int _volumeLevel = 0;
 
-    }
+        [ObservableProperty]
+        private string _logLines = string.Empty;
+
+        public MainWindowViewModel()
+        {
+            _windowTitle = "GoombaCast connected to: yeah";
+        }
+     }
 }
